4331a5b27fa757547093dd5c507174766bea0bbd
wfaoro <wfaoro@umd.edu>
2021-12-29 15:37:03 -0800

    initial commit

---
+++ iris_end_to_end_ML/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for iris_end_to_end_ML project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iris_end_to_end_ML.settings')
+
+application = get_asgi_application()
